1107a6e9852332f28a726ab285168be9e43f525c
gabri00 <gibby.nicchia@gmail.com>
2022-03-06 16:20:39 +0100

    kjhm

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import pygame
 import sys
 
+from player import Player
+
+class Game:
+	def __init__(self):
+		player_sprite = Player((WIDTH / 2, HEIGHT), WIDTH, 5)
+		self.player = pygame.sprite.GroupSingle(player_sprite)
+
+	def run(self):
+		self.player.update()
+		self.player.draw(window)
+
+
+
 if __name__ == '__main__':
 	pygame.init()
 	WIDTH, HEIGHT = 600, 600
 	window = pygame.display.set_mode((WIDTH, HEIGHT))
 	clock = pygame.time.Clock()
+	game = Game()
 
 	while True:
 		for event in pygame.event.get():
@@ -15,4 +29,7 @@ if __name__ == '__main__':
 
 		window.fill((30, 30, 30))
 
+		game.run()
+
+		pygame.display.flip()
 		clock.tick(60)
---
+++ player.py
@@ -0,0 +1,28 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self, pos, constraint, speed):
+		super().__init__()
+		self.image = pygame.image.load('images/ship.png').convert_alpha()
+		self.rect = self.image.get_rect(midbottom = pos)
+		self.speed = speed
+		self.max_x_constraint = constraint
+
+	def move(self):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_RIGHT]:
+			self.rect.x += self.speed
+		elif keys[pygame.K_LEFT]:
+			self.rect.x -= self.speed
+
+	def constraint(self):
+		if self.rect.left <= 0:
+			self.rect.left = 0
+
+		if self.rect.right >= self.max_x_constraint:
+			self.rect.right = self.max_x_constraint
+
+	def update(self):
+		self.move()
+		self.constraint()
