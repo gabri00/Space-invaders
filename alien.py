4933f11e7f8770c05be5b6fecb42fff34e26c5fa
NicolaChiesa <89846244+NicolaChiesa@users.noreply.github.com>
2022-03-08 22:33:09 +0100

    Creato alien
    Mettendo due parametri personalizzati per noi

--- alien.py
+++ alien.py
@@ -1,6 +1,11 @@
 import pygame
 
 class Alien(pygame.sprite.Sprite):
-	def __init__(self, color):
+	# type means the shape of the alien,meanwhile position the layout of the arms
+	def __init__(self, type, position, x, y):
 		super().__init__()
-		self.image
+		file_path = 'images/invader_' + type + position + '.png'
+		# lui aveva messo (file_path).convert_alpha ma a me cosí non andava
+		self.image = pygame.image.load(file_path)
+		
+		self.rect = self.image.get_rect(topleft = (x,y))
